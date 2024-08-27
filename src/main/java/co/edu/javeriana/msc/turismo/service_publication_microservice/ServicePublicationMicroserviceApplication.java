4864d622858b964787ce3bbe93540a26a47ea99c
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-08-27 18:21:17 -0500

    first commit

---
+++ src/main/java/co/edu/javeriana/msc/turismo/service_publication_microservice/ServicePublicationMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package co.edu.javeriana.msc.turismo.service_publication_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServicePublicationMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicePublicationMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/msc/turismo/service_publication_microservice/ServicePublicationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.msc.turismo.service_publication_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicePublicationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
